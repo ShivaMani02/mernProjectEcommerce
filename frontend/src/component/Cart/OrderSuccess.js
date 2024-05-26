8d2d5b04acce4aba079be8fbb3b280cfcc4c6304
hp <kausstubh@getlokalapp.com>
2024-05-26 23:54:23 +0530

    Update cover photo and automatically cart function

--- frontend/src/component/Cart/OrderSuccess.js
+++ frontend/src/component/Cart/OrderSuccess.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import CheckCircleIcon from "@material-ui/icons/CheckCircle";
 import "./orderSuccess.css";
 import { Typography } from "@material-ui/core";
 import { Link } from "react-router-dom";
 
 const OrderSuccess = () => {
+  useEffect(() => {
+    localStorage.removeItem("cartItems");
+  }, []);
+
   return (
     <div className="orderSuccess">
       <CheckCircleIcon />
